2b2094d721b6cc326de9bf6fdcf1917d2701f628
João Vitor <joaovlopes000@gmail.com>
2022-12-21 21:54:38 -0300

    roman to integer finished

--- src/main/java/RomanToInteger.java
+++ src/main/java/RomanToInteger.java
@@ -1,7 +1,7 @@
 public class RomanToInteger {
 
     public static void main(String[] args) {
-        int i = romanToInt("II");
+        int i = romanToInt("MCMXCIV");
         System.out.println(i);
     }
 
@@ -12,12 +12,13 @@ public class RomanToInteger {
         for (char c : s.toCharArray()) {
             Numbers n = Numbers.valueOf(String.valueOf(c).toUpperCase());
             int currentNumber = n.getValue();
-//            if (lastNumber > currentNumber) {
-//                negativeNumbers -= currentNumber;
-//            } else {
-
+            int subtractNumber = n.getSubtractNumber();
             positiveNumbers += currentNumber;
-//            }
+            if (lastNumber == subtractNumber && lastNumber != 0) {
+                positiveNumbers -= currentNumber + lastNumber;
+                negativeNumbers -= subtractNumber;
+                positiveNumbers += currentNumber;
+            }
             lastNumber = n.getValue();
         }
 
@@ -26,26 +27,29 @@ public class RomanToInteger {
 
     public enum Numbers {
 
-        I(1),
-        V(5),
-        X(10),
-        L(50),
-        C(100),
-        D(100),
-        M(1000);
+        I(1, 0),
+        V(5, 1),
+        X(10, 1),
+        L(50, 10),
+        C(100, 10),
+        D(500, 100),
+        M(1000, 100);
 
         private int value;
 
-        Numbers(int value) {
+        private int subtractNumber;
+
+        Numbers(int value, int subtractNumber) {
             this.value = value;
+            this.subtractNumber = subtractNumber;
         }
 
         public int getValue() {
             return value;
         }
 
-        public void setValue(int value) {
-            this.value = value;
+        public int getSubtractNumber() {
+            return subtractNumber;
         }
     }
 }
