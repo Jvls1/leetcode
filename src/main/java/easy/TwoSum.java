b1963e6552c61963977a681e28a70c2b347583c9
Joao Vitor <joaovlopes000@gmail.com>
2023-06-08 21:37:18 -0300

    - TwoSum problem enhancement

--- src/main/java/easy/TwoSum.java
+++ src/main/java/easy/TwoSum.java
@@ -3,25 +3,23 @@ package easy;
 public class TwoSum {
 
     public static void main(String[] args) {
-        int [] testArray= new int[2];
-        testArray[0] = 3;
-        testArray[1] = 3;
+        int [] testArray= new int[4];
+        testArray[0] = 2;
+        testArray[1] = 4;
+        testArray[2] = 11;
+        testArray[3] = 3;
         twoSum(testArray, 6);
     }
 
     public static int[] twoSum(int[] nums, int target) {
 
-        int[] arrayPositions = new int[2];
-
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
                 if(nums[i] + nums[j] == target) {
-                    arrayPositions[0] = i;
-                    arrayPositions[1] = j;
-                    break;
+                    return new int[] {i,j};
                 }
             }
         }
-        return arrayPositions;
+        return new int[] {};
     }
 }
