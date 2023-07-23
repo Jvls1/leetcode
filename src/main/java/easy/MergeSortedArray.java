cac04541d6b55f1cb547cfd5814c3b1a4ddb4a36
Joao Vitor <joaovlopes000@gmail.com>
2023-07-23 17:58:17 -0300

    mergeSortedArray

--- src/main/java/easy/MergeSortedArray.java
+++ src/main/java/easy/MergeSortedArray.java
@@ -1,2 +1,51 @@
-package easy;public class MergeSortedArray {
+package easy;
+
+public class MergeSortedArray {
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[6];
+        nums1[0] = 1;
+        nums1[1] = 2;
+        nums1[2] = 3;
+        nums1[3] = 0;
+        nums1[4] = 0;
+        nums1[5] = 0;
+        int[] nums2 = new int[3];
+        nums2[0] = 2;
+        nums2[1] = 5;
+        nums2[2] = 6;
+        merge(nums1, 3, nums2, 3);
+    }
+    /*
+    Método avançado
+
+    Time complexity: O(m+n)
+    Space complexity: O(1)
+     */
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+
+    /*
+    Metodo mais simples e humilde
+
+    Time complexity: O((m+n)log(m+n))
+    Space complexity: O(1)
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            nums1[m+i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+     */
 }
